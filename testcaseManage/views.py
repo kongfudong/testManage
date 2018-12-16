c00e6804d08b4d322c3ee3be44745504ec9ce0eb
kongfudong <43160715+kongfudong@users.noreply.github.com>
2018-12-16 21:46:08 +0800

    “django第四节”

--- testcaseManage/views.py
+++ testcaseManage/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+# from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.urls import reverse
+from .models import Question, Choice
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world ,this is testcaseManage apps.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('testcaseManage/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'testcaseManage/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'testcaseManage/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'testcaseManage/result.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'testcaseManage/detail.html',
+                      {'question': question, 'error_message': "You didn't select a selected_choice,",
+                       })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('testcaseManage:results', args=(question.id,)))

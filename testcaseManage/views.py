33071436d50099ca34cf3cec5c6b39d898cd9054
kongfudong <43160715+kongfudong@users.noreply.github.com>
2018-12-17 21:59:20 +0800

    testManage

--- testcaseManage/views.py
+++ testcaseManage/views.py
@@ -1,28 +1,27 @@
-from django.http import HttpResponse, HttpResponseRedirect
-# from django.template import loader
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
 
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'testcaseManage/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('testcaseManage/index.html')
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'testcaseManage/index.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'testcaseManage/detail.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'testcaseManage/result.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'testcaseManage/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'testcaseManage/results.html'
 
 
 def vote(request, question_id):
